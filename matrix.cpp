e4bddc097052cce36f738954b8172814d89bd9a8
PhilmacFLy <philmacfly@gmail.com>
2013-06-17 14:06:35 +0200

    Added basic functionallity to the class

---
+++ matrix.cpp
@@ -0,0 +1,61 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2013  PhilmacFLy <philmacfly@binary-kitchen.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "matrix.h"
+
+matrix::matrix(int insize)
+{
+this->size = insize;
+//memory allocated for elements of rows.
+
+
+werte = new int *[size] ;
+
+//memory allocated for  elements of each column.
+
+
+for( int i = 0 ; i < sizeof ; i++ )
+{
+  werte[i] = new int[size];
+  
+}
+
+for(int i=0; i<size; i++)
+    {
+            for(int j=0; j<size; j++)
+            {
+                    werte[i][j]=0;
+            }
+    }
+    
+    werte[0][0] = 1;
+    werte[n-1][n-1] = 1;
+    
+    for (int b=1; b<n-1; b++)
+    {
+        double Rand = werte[0][b-1]-(double)1/(n-1);
+        werte[0][b] = Rand;
+        werte[b][0] = Rand;
+        werte[size-1][(size-1)-b] = Rand;
+        werte[(size-1)-b][size-1] = Rand;
+    }
+}
+
+
+
